bee96d3daf8e6d0112a55f9a42e90908f2f990a6
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2019-07-24 20:19:47 +0200

    Make cartridge size and type data

--- src/memory/cartridge.rs
+++ src/memory/cartridge.rs
@@ -4,14 +4,58 @@ use std::str;
 
 const ROM_BANK_SIZE: usize = 0x4000;
 
-pub struct CartridgeType {
-    pub name: String,
+const CARTRIDGE_TYPES: [CartridgeType; 26] = [
+    CartridgeType{code: 0x00, name:"ROM only", supported: true},
+    CartridgeType{code: 0x01, name:"ROM+MBC1", supported: false},
+    CartridgeType{code: 0x02, name:"ROM+MBC1+RAM", supported: false},
+    CartridgeType{code: 0x03, name:"ROM+MBC1+RAM+BATT", supported: false},
+    CartridgeType{code: 0x05, name:"ROM+MBC2", supported: false},
+    CartridgeType{code: 0x06, name:"ROM+MBC2+BATTERY", supported: false},
+    CartridgeType{code: 0x08, name:"ROM+RAM", supported: false},
+    CartridgeType{code: 0x09, name:"ROM+RAM+BATTERY", supported: false},
+    CartridgeType{code: 0x0B, name:"ROM+MMM01", supported: false},
+    CartridgeType{code: 0x0C, name:"ROM+MMM01+SRAM", supported: false},
+    CartridgeType{code: 0x0D, name:"ROM+MMM01+SRAM+BATT", supported: false},
+    CartridgeType{code: 0x0F, name:"ROM+MBC3+TIMER+BATT", supported: false},
+    CartridgeType{code: 0x10, name:"ROM+MBC3+TIMER+RAM+BATT", supported: false},
+    CartridgeType{code: 0x11, name:"ROM+MBC", supported: false},
+    CartridgeType{code: 0x12, name:"ROM+MBC3+RAM", supported: false},
+    CartridgeType{code: 0x13, name:"ROM+MBC3+RAM+BATT", supported: false},
+    CartridgeType{code: 0x19, name:"ROM+MBC5", supported: false},
+    CartridgeType{code: 0x1A, name:"ROM+MBC5+RAM", supported: false},
+    CartridgeType{code: 0x1B, name:"ROM+MBC5+RAM+BATT", supported: false},
+    CartridgeType{code: 0x1C, name:"ROM+MBC5+RUMBLE", supported: false},
+    CartridgeType{code: 0x1D, name:"ROM+MBC5+RUMBLE+SRAM", supported: false},
+    CartridgeType{code: 0x1E, name:"ROM+MBC5+RUMBLE+SRAM+BATT", supported: false},
+    CartridgeType{code: 0x1F, name:"Pocket Camera", supported: false},
+    CartridgeType{code: 0xFD, name:"Bandai TAMA5", supported: false},
+    CartridgeType{code: 0xFE, name:"Hudson HuC-3", supported: false},
+    CartridgeType{code: 0xFF, name:"Hudson HuC-1", supported: false},
+];
+
+const CARTRIDGE_ROM_SIZES: [CartridgeRomSize; 10] = [
+    CartridgeRomSize {code: 0x00, name:"256Kbit", num_banks: 2},
+    CartridgeRomSize {code: 0x01, name:"512Kbit", num_banks: 4},
+    CartridgeRomSize {code: 0x02, name:"1Mbit", num_banks: 8},
+    CartridgeRomSize {code: 0x03, name:"2Mbit", num_banks: 16},
+    CartridgeRomSize {code: 0x04, name:"4Mbit", num_banks: 32},
+    CartridgeRomSize {code: 0x05, name:"8Mbit", num_banks: 64},
+    CartridgeRomSize {code: 0x06, name:"16Mbit", num_banks: 128},
+    CartridgeRomSize {code: 0x52, name:"9Mbit", num_banks: 72},
+    CartridgeRomSize {code: 0x53, name:"10Mbit", num_banks: 80},
+    CartridgeRomSize {code: 0x54, name:"12Mbit", num_banks: 96},
+];
+
+pub struct CartridgeType<'a> {
+    pub name: &'a str,
     pub supported: bool,
+    pub code: u8,
 }
 
-pub struct CartridgeRomSize {
-    pub name: String,
+pub struct CartridgeRomSize<'a> {
+    pub name: &'a str,
     pub num_banks: u8,
+    pub code: u8,
 }
 
 pub struct Cartridge {
@@ -77,51 +121,23 @@ impl Cartridge {
         cartridge
     }
 
-    pub fn get_cartridge_type(&self) -> CartridgeType {
-        match self.rom_banks[0][0x0147] {
-            0x00 => CartridgeType{name:"ROM only".to_string(), supported: true},
-            0x01 => CartridgeType{name:"ROM+MBC1".to_string(), supported: false},
-            0x02 => CartridgeType{name:"ROM+MBC1+RAM".to_string(), supported: false},
-            0x03 => CartridgeType{name:"ROM+MBC1+RAM+BATT".to_string(), supported: false},
-            0x05 => CartridgeType{name:"ROM+MBC2".to_string(), supported: false},
-            0x06 => CartridgeType{name:"ROM+MBC2+BATTERY".to_string(), supported: false},
-            0x08 => CartridgeType{name:"ROM+RAM".to_string(), supported: false},
-            0x09 => CartridgeType{name:"ROM+RAM+BATTERY".to_string(), supported: false},
-            0x0B => CartridgeType{name:"ROM+MMM01".to_string(), supported: false},
-            0x0C => CartridgeType{name:"ROM+MMM01+SRAM".to_string(), supported: false},
-            0x0D => CartridgeType{name:"ROM+MMM01+SRAM+BATT".to_string(), supported: false},
-            0x0F => CartridgeType{name:"ROM+MBC3+TIMER+BATT".to_string(), supported: false},
-            0x10 => CartridgeType{name:"ROM+MBC3+TIMER+RAM+BATT".to_string(), supported: false},
-            0x11 => CartridgeType{name:"ROM+MBC".to_string(), supported: false},
-            0x12 => CartridgeType{name:"ROM+MBC3+RAM".to_string(), supported: false},
-            0x13 => CartridgeType{name:"ROM+MBC3+RAM+BATT".to_string(), supported: false},
-            0x19 => CartridgeType{name:"ROM+MBC5".to_string(), supported: false},
-            0x1A => CartridgeType{name:"ROM+MBC5+RAM".to_string(), supported: false},
-            0x1B => CartridgeType{name:"ROM+MBC5+RAM+BATT".to_string(), supported: false},
-            0x1C => CartridgeType{name:"ROM+MBC5+RUMBLE".to_string(), supported: false},
-            0x1D => CartridgeType{name:"ROM+MBC5+RUMBLE+SRAM".to_string(), supported: false},
-            0x1E => CartridgeType{name:"ROM+MBC5+RUMBLE+SRAM+BATT".to_string(), supported: false},
-            0x1F => CartridgeType{name:"Pocket Camera".to_string(), supported: false},
-            0xFD => CartridgeType{name:"Bandai TAMA5".to_string(), supported: false},
-            0xFE => CartridgeType{name:"Hudson HuC-3".to_string(), supported: false},
-            0xFF => CartridgeType{name:"Hudson HuC-1".to_string(), supported: false},
-            _ => panic!("Invalid cartridge type"),
+    pub fn get_cartridge_type(&self) -> &CartridgeType {
+        let type_code = self.rom_banks[0][0x0147];
+
+        for cartridge_type_iterator in CARTRIDGE_TYPES.iter() {
+            if cartridge_type_iterator.code == type_code { return cartridge_type_iterator }
         }
+
+        panic!("Invalid cartridge type code: {:X?}", type_code);
     }
 
-    pub fn get_rom_size(&self) -> CartridgeRomSize {
-        match self.rom_banks[0][0x0148] {
-            0x00 => CartridgeRomSize {name:"256Kbit".to_string(), num_banks: 2},
-            0x01 => CartridgeRomSize {name:"512Kbit".to_string(), num_banks: 4},
-            0x02 => CartridgeRomSize {name:"1Mbit".to_string(), num_banks: 8},
-            0x03 => CartridgeRomSize {name:"2Mbit".to_string(), num_banks: 16},
-            0x04 => CartridgeRomSize {name:"4Mbit".to_string(), num_banks: 32},
-            0x05 => CartridgeRomSize {name:"8Mbit".to_string(), num_banks: 64},
-            0x06 => CartridgeRomSize {name:"16Mbit".to_string(), num_banks: 128},
-            0x52 => CartridgeRomSize {name:"9Mbit".to_string(), num_banks: 72},
-            0x53 => CartridgeRomSize {name:"10Mbit".to_string(), num_banks: 80},
-            0x54 => CartridgeRomSize {name:"12Mbit".to_string(), num_banks: 96},
-            _ => panic!("Invalid rom size"),
+    pub fn get_rom_size(&self) -> &CartridgeRomSize {
+        let type_size = self.rom_banks[0][0x0148];
+
+        for cartridge_size_iterator in CARTRIDGE_ROM_SIZES.iter() {
+            if cartridge_size_iterator.code == type_size { return cartridge_size_iterator }
         }
+
+        panic!("Invalid rom size code: {:X?}", type_size);
     }
 }
